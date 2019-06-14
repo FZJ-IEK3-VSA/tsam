31c06517c7cae38e59570a3c970fb8b6ab0ffc23
l-kotzur <l.kotzur@fz-juelich.de>
2019-06-14 18:49:52 +0200

    add new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(dir_path,'README.md'), "r") as fh:
 
 setuptools.setup(
     name='tsam',
-    version='1.0.3',
+    version='1.0.4',
     author='Leander Kotzur',
     author_email='l.kotzur@fz-juelich.de',
     description='Time series aggregation module (tsam) to create typical periods',
