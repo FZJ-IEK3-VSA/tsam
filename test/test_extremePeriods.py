ef84301888a60fe5a0ea0f981075473e547aff3f
maximilian-hoffmann <max.hoffmann@fz-juelich.de>
2020-05-09 19:59:49 +0200

    Test for addMeanMax and addMeanMin added

--- test/test_extremePeriods.py
+++ test/test_extremePeriods.py
@@ -38,5 +38,20 @@ def test_extremePeriods():
     np.testing.assert_array_less(aggregation2.accuracyIndicators().loc['GHI', 'RMSE'],
                                  aggregation3.accuracyIndicators().loc['GHI', 'RMSE'])
 
+    # check if addMeanMax and addMeanMin are working
+    aggregation4 = tsam.TimeSeriesAggregation(raw, noTypicalPeriods=noTypicalPeriods, hoursPerPeriod=hoursPerPeriod,
+                                              clusterMethod='hierarchical', rescaleClusterPeriods=False,
+                                              extremePeriodMethod='append', addMeanMax=['GHI'], addMeanMin=['GHI'])
+
+    origData = aggregation4.predictOriginalData()
+
+    np.testing.assert_array_almost_equal(raw.groupby(np.arange(len(raw)) // 24).mean().max().loc['GHI'],
+                                         origData.groupby(np.arange(len(origData)) // 24).mean().max().loc['GHI'],
+                                         decimal=6)
+
+    np.testing.assert_array_almost_equal(raw.groupby(np.arange(len(raw)) // 24).mean().min().loc['GHI'],
+                                         origData.groupby(np.arange(len(origData)) // 24).mean().min().loc['GHI'],
+                                         decimal=6)
+
 if __name__ == "__main__":
     test_extremePeriods()
