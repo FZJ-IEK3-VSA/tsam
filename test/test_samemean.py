103d3f570b856b214ba2a290d5aa2d693d53dd55
l-kotzur <leander.kotzur@googlemail.com>
2022-10-17 16:10:31 +0200

    remove falsy test

--- test/test_samemean.py
+++ test/test_samemean.py
@@ -42,43 +42,6 @@ def test_samemean():
         raw.mean(), rearangedData[raw.columns].mean(), decimal=4
     )
 
-def test_samemean_with_segmentation():
-
-    raw = pd.read_csv(
-        os.path.join(os.path.dirname(__file__), "..", "examples", "testdata.csv"),
-        index_col=0,
-    )
-    # get all columns as floats to avoid warning
-    for col in raw.columns:
-        raw[col] = raw[col].astype(float)
-
-    starttime = time.time()
-
-    aggregation = tsam.TimeSeriesAggregation(
-        raw,
-        noTypicalPeriods=12,
-        hoursPerPeriod=24,
-        clusterMethod="k_means",
-        sameMean=True,
-        segmentation=True,
-        noSegments=8,
-    )
-
-    typPeriods = aggregation.createTypicalPeriods()
-    print("Clustering took " + str(time.time() - starttime))
-
-    # test if the normalized time series all have the same mean
-    means = aggregation.normalizedTimeSeries.mean().values
-    np.testing.assert_allclose(means, np.array([means[0]] * len(means)), rtol=1e-5)
-
-    # repredict the original data
-    rearangedData = aggregation.predictOriginalData()
-
-    # test if the mean fits the mean of the raw time series --> should always hold for k-means independent from sameMean True or False
-    np.testing.assert_array_almost_equal(
-        raw.mean(), rearangedData[raw.columns].mean(), decimal=4
-    )
-
 
 
 
